136ce63bc6892be12fbae5dec9180701ff5a3d69
Zachariah <1019559@student.sae.edu.au>
2020-07-14 16:44:34 +1000

    Changes to movement script
    Removal of w and s key's to move character

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -16,13 +16,13 @@ public class playerMovement : MonoBehaviour
 	//Update is called once per frame
 	void FixedUpdate() {
 
-		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w")) {
+		/*if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w")) {
 			transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
 		} else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift)) {
 			transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
 		} else if (Input.GetKey("s")) {
 			transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-		}
+		}*/
 
 		if (Input.GetKey("a") && !Input.GetKey("d")) {
 			transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
