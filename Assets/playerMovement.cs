7a0537c103dc1de646b7ed1205b8740ba2e74e36
Zachariah <1019559@student.sae.edu.au>
2020-07-14 11:19:57 +1000

    Changes to the Movement Script
    Using new script for the movement, changes to the scene and new focus for the movement

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -9,25 +9,37 @@ public class playerMovement : MonoBehaviour
 	public float movementSpeed;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 
 	}
 
 	//Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate() {
 
-		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey ("w")) {
-			transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
-		}	else if (Input.GetKey ("w") && !Input.GetKey (KeyCode.LeftShift)) {
-			transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
-		}	else if (Input.GetKey ("s")) {
-			transform.position -= transform.TransformDirection (Vector3.forward) * Time.deltaTime * movementSpeed;
+		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w")) {
+			transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+		} else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift)) {
+			transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+		} else if (Input.GetKey("s")) {
+			transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
 		}
 
-		if (Input.GetKey ("a") && !Input.GetKey ("d")) {
-				transform.position += transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-			} else if (Input.GetKey ("d") && !Input.GetKey ("a")) {
-				transform.position -= transform.TransformDirection (Vector3.left) * Time.deltaTime * movementSpeed;
-			}
+		if (Input.GetKey("a") && !Input.GetKey("d")) {
+			transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+		} else if (Input.GetKey("d") && !Input.GetKey("a")) {
+			transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
 		}
+		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("a") && !Input.GetKey("d"))
+        {
+			transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("d") && !Input.GetKey("a"))
+        {
+			transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * movementSpeed * 2.5f;
+		}
+			if (Input.GetKey(KeyCode.Space))
+        {
+			transform.position += transform.TransformDirection(Vector3.up) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+	}
 }
