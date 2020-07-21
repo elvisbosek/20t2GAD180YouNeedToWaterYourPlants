c6f16ae37644635e051754c0fc6d18fc3b244f43
Zachariah <1019559@student.sae.edu.au>
2020-07-21 16:51:31 +1000

    Changes to Player Code
    Collision code has been added to the player code, this allows for the interaction of player/well and plant to continue, well code is done and ready, plant code needs to be done, code in playerMovement  has been updated to show the collision system and relevant code for the plant watering when that is done

---
+++ Assets/eventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventManager : MonoBehaviour
+{
+    void start()
+    {
+        
+    }
+    
+    
+    public delegate void wateringSunflower();
+    public wateringSunflower myDelegate;
+    //public static event wateringSunflower watered;
+
+    /*public static void waterSunflower ()
+    {
+        eventManager.watered();
+    }*/
+   
+}
--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+//using System.Diagnostics;
+//using System.Diagnostics.Eventing.Reader;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
 	public float movementSpeed;
+	public bool wellZone = false;
+	public bool canFull = false;
+	public PlantWatering currentPlant;
 
 	// Use this for initialization
-	void Start() {
+	void Start() 
+	{
 
 	}
+	void Update()
+    {
+		if (wellZone == true && !canFull == true)
+		{
+			if (Input.GetKey("e"))
+			{
+				canFull = true;
+			}
+		}
+		if(currentPlant != null && canFull == true)
+        {
+            //water plant
+            if (Input.GetKey("e"))
+            {
+				canFull = false;
+			}
+			
+        }
+	}
 
+	
 	//Update is called once per frame
 	void FixedUpdate() {
 
@@ -37,9 +64,33 @@ public class playerMovement : MonoBehaviour
         {
 			transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * movementSpeed * 2.5f;
 		}
-			if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKey(KeyCode.Space))
         {
 			transform.position += transform.TransformDirection(Vector3.up) * Time.deltaTime * movementSpeed * 2.5f;
+        } 
+	}
+
+	public void OnTriggerEnter(Collider otherObject)
+    {
+		if(otherObject.gameObject.name == "Well")
+        {
+			wellZone = true;
         }
+		if(otherObject.gameObject.GetComponent<PlantWatering>())
+        {
+			currentPlant = otherObject.gameObject.GetComponent<PlantWatering>();
+
+		}
+    }
+	public void OnTriggerExit(Collider otherObject)
+    {
+		if(otherObject.gameObject.name == "Well")
+        {
+			wellZone = false;
+        }
+		if (otherObject.gameObject.GetComponent<PlantWatering>())
+		{
+			currentPlant = null;
+		}
 	}
 }
